455f93bfa83ede2cb969766af10edd9697e86851
maks <indcheat@mail.ru>
2024-04-24 11:33:11 +0300

    Доработка сайта

---
+++ actions-with-js/openMenu.js
@@ -0,0 +1,9 @@
+function openMenu(){
+    document.getElementById('menu__right-panel').style.display = "block";
+    document.getElementById('header-cap__about-us').style.paddingRight = "63%";
+}
+
+function closeMenu() {
+    document.getElementById('menu__right-panel').style.display = "none";
+    document.getElementById('header-cap__about-us').style.paddingRight = "0";
+}
